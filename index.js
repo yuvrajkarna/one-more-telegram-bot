c5c87d36aad9f6d4307f7f4161612ce7937f7315
Yuvraj Karna <yuvrajkarna2000@gmail.com>
2024-05-08 17:06:45 +0530

    change whole code trying another way

--- index.js
+++ index.js
@@ -1,60 +1,31 @@
-/**
- * This example demonstrates setting up a webook, and receiving
- * updates in your express app
- */
-/* eslint-disable no-console */
-require('dotenv').config();
-
-const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
-const url = 'https://one-more-telegram-bot.vercel.app';
-const port = process.env.PORT;
-
-const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
-
-// No need to pass any parameters as we will handle the updates with Express
-const bot = new TelegramBot(TOKEN);
-
-// This informs the Telegram servers of the new webhook.
-bot.setWebHook(`${url}/bot${TOKEN}`);
-
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+const express = require("express");
 const app = express();
-
-// parse the updates to JSON
-app.use(express.json());
-
-// We are receiving updates at the route below!
-app.post(`/bot${TOKEN}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
-
+require('dotenv').config();
 app.get("/", (req, res) => {
-  res.send("Bot is on from server..")
+    res.send("Bot is alive");
 });
 
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-    console.log("Hello from /somethings");
-  
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-  
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-  });
-
-// Just to ping!
-bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'I am alive!');
-});
-
-
-// Start Express Server
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Express server is listening on ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const userInput = msg.text;
+    const msg_id = msg.message_id;
+
+    sent_message = await bot.sendMessage(chatId, userInput, {
+        reply_to_message_id: msg_id,
+    });
+
+});
+
+module.exports = app;
